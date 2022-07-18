d04bb3b5c0fb202f4d12d0e63683bfb18a3aef4a
Saqo Harutyunyan <saqo.harutyunyan.ait@gmail.com>
2022-07-19 02:44:31 +0400

    react

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+function App() {
+  return React.createElement(
+    "div",
+    { className: "App" },
+    React.createElement(
+      "header",
+      { className: "App-header" },
+
+      React.createElement("img", {
+        src: logo,
+        className: "App-logo",
+        alt: "logo"
+      }),
+      React.createElement("p", null, `Helo World`),
+      React.createElement(
+        "a",
+        {
+          className: "App-link",
+          href: "https://reactjs.org",
+          target: "_blank",
+          rel: "noopener noreferrer"
+        },
+        "Learn React"
+      )
+    )
+  );
+}
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>Hello World</p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+
+export default App;
+
+
